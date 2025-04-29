e0020d9821043eadb7f0b631569ee47428d576ee
Rafael Sousa <rafaelsousa@MacBook-Air-de-Rafael.local>
2025-04-28 22:46:51 -0300

    feat: change gtm

--- app/components/GoogleTagManager.tsx
+++ app/components/GoogleTagManager.tsx
@@ -4,13 +4,13 @@ import { useEffect } from "react";
 import TagManager from "react-gtm-module";
 import useUserIP from "../hooks/useUserIP";
 
-const GTM_ID = "GTM-KQWLTDD"; // Substitua pelo seu ID do GTM
 
 const GoogleTagManager = () => {
     const userIp = useUserIP(); // Captura o IP no carregamento
     console.log('meu ip', userIp);
     useEffect(() => {
-        TagManager.initialize({ gtmId: GTM_ID });
+        TagManager.initialize({ gtmId: 'GTM-NC7GGZBX' });
+        TagManager.initialize({ gtmId: 'GTM-KQWLTDD' });
     }, []);
 
     return null; // Esse componente não precisa renderizar nada
